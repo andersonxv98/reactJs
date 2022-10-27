4865da927ce649ada6fe780c2f2ffeab819eafd7
andersonxv98 <72809650+andersonxv98@users.noreply.github.com>
2022-10-26 21:55:16 -0300

    componentização
    comp

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Titulo from './Titulo';
 import Paragrafo from './Paragrafo'
+import DataEHora from './DataEHora';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+function tick(){
+  root.render(
     <div>
       <Titulo texto="Esse é um Título"/>
       <Paragrafo/>
       <Paragrafo/>
       <Paragrafo/>
       <Paragrafo/>
+      <DataEHora/>
     </div>
-);
+  )
+};
+
+
+setInterval(tick, 1000);
 
 
